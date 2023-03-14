a6a7429ef164fef7d0ce9cc1ee41bc77bee16aaa
najoua belhaj <najouabelhaj7@gmail.com>
2023-03-14 17:28:30 +0100

    commit

---
+++ E-commerce/wp-content/plugins/pymntpl-paypal-woocommerce/build/js/frontend-commons.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'e8b69c261217c4d28307');
---
+++ E-commerce/wp-content/plugins/pymntpl-paypal-woocommerce/packages/blocks/build/blocks-commons.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '6a07e573d696cd10a05f');
